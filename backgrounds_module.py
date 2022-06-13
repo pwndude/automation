e7abef41f098af2c3d31a384f7b46899d43ffcee
Joel Smith <smithjoelt@gmail.com>
2022-06-13 02:13:44 -0700

    iterated until acheived results

--- backgrounds_module.py
+++ backgrounds_module.py
@@ -1,9 +1,34 @@
-# use WMI library
-import wmi
+# use requests
+import requests as r
 
-# prints all properties
-wmi.WMI().Win32_Process.properties.keys()
+# use shutil
+import shutil as sh
 
-# prints all methods
-wmi.WMI().Win32_Process.methods.keys()
+# use ctypes
+import ctypes as c
 
+# use time as t
+import time as t
+
+
+# # get a random background image from unsplash
+# response = r.get("https://source.unsplash.com/random/1920x1080")
+
+# # save the image to a file and move to C:/Users/Public/Pictures/Backgrounds/background.jpg
+# sh.copyfileobj(response.raw, open("C:/Users/Public/Pictures/Backgrounds/background.jpg", "wb"))
+
+# use ctypes to set the background
+
+# # repeat every 30 minutes
+# t.sleep(1800)
+
+response = r.get("https://source.unsplash.com/random/1920x1080")
+
+# complete all actions every 30 minutes
+def loop_for_eternity():
+    while True:
+        sh.copyfileobj(response.raw, open("C:/Users/Public/Pictures/Backgrounds/background.jpg", "wb"))
+        c.windll.user32.SystemParametersInfoW(20, 0, "C:/Users/Public/Pictures/Backgrounds/background.jpg")
+        t.sleep(1800)
+    
+loop_for_eternity()
