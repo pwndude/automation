9e98247dcf2b01bc716c6dc96a1a4d98daa82d82
Joel Smith <smithjoelt@gmail.com>
2022-06-13 02:17:57 -0700

    Finished

--- backgrounds_module.py
+++ backgrounds_module.py
@@ -22,13 +22,13 @@ import time as t
 # # repeat every 30 minutes
 # t.sleep(1800)
 
-response = r.get("https://source.unsplash.com/random/1920x1080")
+response = r.get("https://source.unsplash.com/random/1920x1080") # get a random background image from unsplash
 
 # complete all actions every 30 minutes
-def loop_for_eternity():
-    while True:
-        sh.copyfileobj(response.raw, open("C:/Users/Public/Pictures/Backgrounds/background.jpg", "wb"))
-        c.windll.user32.SystemParametersInfoW(20, 0, "C:/Users/Public/Pictures/Backgrounds/background.jpg")
-        t.sleep(1800)
+def loop_for_eternity(): 
+    while True: 
+        sh.copyfileobj(response.raw, open("C:/Users/Public/Pictures/Backgrounds/background.jpg", "wb")) # save the image to a file and move to C:/Users/Public/Pictures/Backgrounds/background.jpg
+        c.windll.user32.SystemParametersInfoW(20, 0, "C:/Users/Public/Pictures/Backgrounds/background.jpg") # set the background
+        t.sleep(1800) # repeat every 30 minutes
     
-loop_for_eternity()
+loop_for_eternity() # start the loop
